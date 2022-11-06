014a59c28ea707a6748a6a2c712c021b35a01924
quocthinh861 <quocthinh861@gmail.com>
2022-11-06 14:17:20 +0700

    update routing path

--- client/src/views/AppMap/AppMap.js
+++ client/src/views/AppMap/AppMap.js
@@ -1,27 +1,31 @@
 /* eslint-disable prettier/prettier */
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
+import RoutineMachine from './RoutingMachine';
 
 const position = [10.7724483, 106.6582936];
 
+const maps = {
+  base: "https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=gGCh0FCy8fIynFL9Aksz"
+};
+
 const AppMap = () => {
-    const [visible, setVisible] = useState(false)
+  
+  //Set initial state for the map
+  const [map, setMap] = useState(null);
 
   return (
     <>
       <MapContainer
         center={position}
         zoom={16}
+        whenReady={map => setMap(map)}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=gGCh0FCy8fIynFL9Aksz"
+          url={maps.base}
         />
-        <Marker position={position}>
-          <Popup>
-            Tui đang đứng ở đây nè 
-          </Popup>
-        </Marker>
+        <RoutineMachine />
       </MapContainer>
     </>
   )
---
+++ client/src/views/AppMap/RoutingMachine.js
@@ -0,0 +1,32 @@
+import L from 'leaflet'
+import { createControlComponent } from '@react-leaflet/core'
+import 'leaflet-routing-machine'
+
+const createRoutineMachineLayer = (props) => {
+  const instance = L.Routing.control({
+    waypoints: [
+      L.latLng(33.52001088075479, 36.26829385757446),
+      L.latLng(33.50546582848033, 36.29547681726967),
+    ],
+    lineOptions: {
+      styles: [{ color: '#6FA1EC', weight: 4 }],
+    },
+    show: false,
+    addWaypoints: false,
+    routeWhileDragging: true,
+    draggableWaypoints: true,
+    fitSelectedRoutes: true,
+    showAlternatives: false,
+  })
+
+  instance.on('routeselected', function (e) {
+    var routes = e.route
+    console.log(routes)
+  })
+
+  return instance
+}
+
+const RoutingMachine = createControlComponent(createRoutineMachineLayer)
+
+export default RoutingMachine
