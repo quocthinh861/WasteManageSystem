40bc076b644d4f316369e4aa3df9041e5f8f3283
quocthinh861 <quocthinh861@gmail.com>
2022-11-06 19:28:00 +0700

    update

--- client/src/views/AppMap/AppMap.js
+++ client/src/views/AppMap/AppMap.js
@@ -9,6 +9,36 @@ const maps = {
   base: "https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=gGCh0FCy8fIynFL9Aksz"
 };
 
+const vextexMap = {
+  A: [10.777823448181152, 106.65606689453125],
+  B: [10.748285429708451, 106.63473038607164],
+  C: [10.747325095957946, 106.64353901039668],
+  D: [10.754058881386277, 106.65132009782434],
+}
+
+const routeArray = [
+  {
+    start: vextexMap.A,
+    end: vextexMap.B,
+    color: 'red'
+  },
+  {
+    start: vextexMap.B,
+    end: vextexMap.C,
+    color: 'blue'
+  },
+  {
+    start: vextexMap.C,
+    end: vextexMap.D,
+    color: 'green'
+  },
+  {
+    start: vextexMap.D,
+    end: vextexMap.A,
+    color: 'purple'
+  }
+]
+
 const AppMap = () => {
   
   //Set initial state for the map
@@ -25,7 +55,11 @@ const AppMap = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url={maps.base}
         />
-        <RoutineMachine />
+        {
+          routeArray.map((x, index) => (
+            <RoutineMachine routeInfo={x} key={index} />
+          ))
+        }
       </MapContainer>
     </>
   )
--- client/src/views/AppMap/RoutingMachine.js
+++ client/src/views/AppMap/RoutingMachine.js
@@ -2,17 +2,17 @@ import L from 'leaflet'
 import { createControlComponent } from '@react-leaflet/core'
 import 'leaflet-routing-machine'
 
-const createRoutineMachineLayer = (props) => {
+const createRoutineMachineLayer = ({ routeInfo, ...props }) => {
   const instance = L.Routing.control({
     waypoints: [
-      L.latLng(33.52001088075479, 36.26829385757446),
-      L.latLng(33.50546582848033, 36.29547681726967),
+      L.latLng(routeInfo.start[0], routeInfo.start[1]),
+      L.latLng(routeInfo.end[0], routeInfo.end[1]),
     ],
     lineOptions: {
-      styles: [{ color: '#6FA1EC', weight: 4 }],
+      styles: [{ color: routeInfo.color, weight: 4 }],
     },
     show: false,
-    addWaypoints: false,
+    addWaypoints: true,
     routeWhileDragging: true,
     draggableWaypoints: true,
     fitSelectedRoutes: true,
@@ -21,7 +21,6 @@ const createRoutineMachineLayer = (props) => {
 
   instance.on('routeselected', function (e) {
     var routes = e.route
-    console.log(routes)
   })
 
   return instance
